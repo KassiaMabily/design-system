9695059550c748d1b02d668c0eec803fdf07af2f
Kassia Fraga <kassiafraga7@gmail.com>
2023-09-04 17:05:05 -0300

    storybook

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,13 @@
+import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+})
+
+export function App() {
+  return <Button>Hello World</Button>
+}
